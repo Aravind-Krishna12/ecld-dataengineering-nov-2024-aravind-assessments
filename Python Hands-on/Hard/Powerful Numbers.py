1e969a4fddc6a5fc7e57417128a4f685bdc5766c
Aravind-Krishna12 <aravind.krishna@edstem.com>
2024-11-26 21:12:56 +0530

    Committed Poweful Numbers python file

---
+++ Python Hands-on/Hard/Powerful Numbers.py
@@ -0,0 +1,18 @@
+def is_powerful(n):
+	number = n
+	factors = []
+	d = 2
+	b=0
+	while (n>=d):
+		if n%d == 0 :
+			factors.append(d)
+			n=n/d
+		else :
+			d+=1
+	set_factors = set(factors)
+	for i in set_factors:
+		if number%(i**2) == 0 :
+			b+=1
+		
+	return True if len(set_factors)==b else False
+			
