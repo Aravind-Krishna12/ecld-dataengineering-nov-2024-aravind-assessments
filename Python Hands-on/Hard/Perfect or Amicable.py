5ea87c362aee8b9aacbfb9920dc3267cb810c5f4
Aravind-Krishna12 <aravind.krishna@edstem.com>
2024-11-27 14:59:00 +0530

    Committed Perfect or Amicable-Hard

---
+++ Python Hands-on/Hard/Perfect or Amicable.py
@@ -0,0 +1,19 @@
+#Given a positive number x, if all the positive divisors of x (excluding x) add up to x, 
+#then x is said to be a perfect number.
+#Given a positive number x, if all the positive divisors of x add up to a second number y, 
+#and all the positive divisors of y add up to x, then x and y are said to be a pair of amicable numbers.
+
+def num_type(n):
+	def sum_div(num):
+		lst = []
+		for i in range(1,num) :
+			if num%i == 0:
+				lst.append(i)
+		return sum(lst)
+	s = sum_div(n)
+	if s == n:
+		return "Perfect"
+	elif sum_div(s)==n :
+		return "Amicable"
+	else :
+		return "Neither"
