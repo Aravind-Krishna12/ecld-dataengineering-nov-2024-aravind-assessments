bf4dc09d9c566716601297c8fa2fb414e302f846
Aravind-Krishna12 <aravind.krishna@edstem.com>
2024-11-28 08:19:49 +0530

    Edited Majority votes

---
+++ Python Hands-on/Hard/Friday The 13th.py
@@ -0,0 +1,9 @@
+'''Given the month and year as numbers, return whether that month contains a Friday 13th.
+
+Examples
+has_friday_13(3, 2020) ➞ True
+
+has_friday_13(10, 2017) ➞ True
+
+has_friday_13(1, 1985) ➞ False'''
+
--- Python Hands-on/Hard/Majority Votes.py
+++ Python Hands-on/Hard/Majority Votes.py
@@ -1,6 +1,7 @@
 #Check whether the count of unique element is greater than n/2(n - length of the list)
 
 def majority_vote(lst):
+      print(lst)
       len_list = len(lst)
       set_list = set(lst)
       a = []
@@ -9,4 +10,13 @@ def majority_vote(lst):
           count = lst.count(item)
           if count > len_list/2:
               return item 
-      return None   
+      return None 
+  
+lst_1 =input("Enter the elements separated by comma : ")
+lst  = lst_1.split(",")
+print(majority_vote(lst))
+
+
+
+#user_list = [int(x) for x in input(&quot;Enter elements separated by space:&quot;).split('')]
+#print(user_list)
