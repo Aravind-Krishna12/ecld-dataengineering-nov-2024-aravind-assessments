d38b699432f28c98d15064312ec3e0f74a413b8d
Aravind-Krishna12 <aravind.krishna@edstem.com>
2024-12-10 10:15:11 +0530

    Edited file names

--- exercises/easy/NumberofSquares.py
+++
@@ -1,8 +0,0 @@
-#Create a function that calculates the number of different squares in an n * n square grid.
-#sum of squares
-
-def numberSquares(n) :
-	if n == 0:
-		return n
-	else :
-		return n**2 + numberSquares(n-1) 
--- exercises/medium/ValidDivision.py
+++
@@ -1,8 +0,0 @@
-#Given a math division ,check whether its a valid division
-
-
-
-def valid_division(d):
-	d_list = d.split("/")
-	return "invalid" if int(d_list[1]) == 0 else True if int(d_list[0])%int(d_list[1]) == 0 else True if int(d_list[0]) == 0  else False
-	
